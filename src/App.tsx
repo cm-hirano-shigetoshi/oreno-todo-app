5be54f3e616c3298283b941ac5496f9878c1b3fb
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-09-15 00:50:50 +0900

    ChakraUIベースで書き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,76 @@
-import {Page} from './Page';
+import { useState, useEffect } from "react";
+import { ChakraProvider, Button, Input, Flex } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
+import { HeaderLayout } from "./components/templates/HeaderLayout";
+
+type Todo = {
+  id: string;
+  order: string;
+  todo: string;
+  taskcode: string;
+  estimate: string;
+  elapsed: string;
+  memo: string;
+};
 
 function App() {
-    return <Page />;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    setTodos([
+      {
+        id: "1",
+        order: "1",
+        todo: "あれやる",
+        taskcode: "c123",
+        estimate: "120",
+        elapsed: "90",
+        memo: "hogehoge",
+      },
+      {
+        id: "1",
+        order: "1",
+        todo: "あれやる",
+        taskcode: "c123",
+        estimate: "120",
+        elapsed: "90",
+        memo: "hogehoge",
+      },
+    ]);
+  }, []);
+
+  return (
+    <>
+      <ChakraProvider theme={theme}>
+        <HeaderLayout>
+          {todos.map((todo) => {
+            return (
+              <>
+                <Flex key={todo.id}>
+                  <Input h="100%" value={todo.order} />
+                  <Input h="100%" value={todo.todo} />
+                  <Input h="100%" value={todo.taskcode} />
+                  <Input h="100%" value={todo.estimate} />
+                  <Button h="100%" colorScheme={"teal"}>
+                    start
+                  </Button>
+                  <Input h="100%" value={todo.elapsed} />
+                  <Button h="100%" colorScheme={"teal"}>
+                    done
+                  </Button>
+                  <Input h="100%" value={todo.memo} />
+                  <Button h="100%" colorScheme={"teal"}>
+                    trash
+                  </Button>
+                </Flex>
+              </>
+            );
+          })}
+        </HeaderLayout>
+      </ChakraProvider>
+    </>
+  );
 }
 
 export default App;
-
---
+++ src/components/organisms/layout/Header.tsx
@@ -0,0 +1,42 @@
+import { memo, FC } from "react";
+import { useDisclosure, Flex, Link, Box, Heading } from "@chakra-ui/react";
+
+import { MenuIconButton } from "../../atoms/button/MenuIconButton";
+import { MenuDrawer } from "../../molecules/MenuDrawer";
+
+export const Header: FC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
+        align="center"
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
+      >
+        <Flex>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            TODOリスト
+          </Heading>
+        </Flex>
+        <Flex
+          align="cneter"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Box pr={4}>
+            <Link>ユーザー一覧</Link>
+          </Box>
+          <Box pr={4}>
+            <Link>設定</Link>
+          </Box>
+        </Flex>
+        <MenuIconButton onOpen={onOpen} />
+      </Flex>
+      <MenuDrawer isOpen={isOpen} onClose={onClose} />
+    </>
+  );
+});

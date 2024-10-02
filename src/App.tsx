b0389a98ef61e256db92cb57d5b5c229da2198fa
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-10-03 00:49:48 +0900

    debounceを使った保存に修正

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,25 @@ import theme from "./theme/theme";
 import { HeaderLayout } from "./components/templates/HeaderLayout";
 import { Todo, TodoItem } from "./components/organisms/todo/TodoItem";
 
+function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const debouncedTodos = useDebounce(todos, 500);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,19 +35,20 @@ function App() {
   }, []);
 
   const handleTodoChange = (id: string, newText: string) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, todo: newText } : todo
+      )
+    );
+  };
+
+  useEffect(() => {
     const saveData = async (content: string) => {
       const filePath = "/tmp/sample.json";
       await window.electronAPI.writeFile(filePath, content);
     };
-
-    setTodos((prevTodos) => {
-      const newTodos = prevTodos.map((todo) =>
-        todo.id === id ? { ...todo, todo: newText } : todo
-      );
-      saveData(JSON.stringify(newTodos, null, "  "));
-      return newTodos;
-    });
-  };
+    saveData(JSON.stringify(debouncedTodos, null, "  "));
+  }, [debouncedTodos]);
 
   return (
     <>

1e16a49ea61c8ba1f294045ca19d340b4aa913ba
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-11-03 23:09:41 +0900

    function形式からアロー関数式へ

--- src/logic/Time.ts
+++ src/logic/Time.ts
@@ -4,14 +4,14 @@ const HESITATION_SECONDS = 10;
 
 export type Time = { start: string; end: string };
 
-export function startTimer(times: Time[], dt: string): Time[] {
+export const startTimer = (times: Time[], dt: string): Time[] => {
   if (times.length === 0 || times[times.length - 1].end !== null) {
     times.push({ start: dt, end: null });
   }
   return times;
-}
+};
 
-export function stopTimer(times: Time[], dt: string): Time[] {
+export const stopTimer = (times: Time[], dt: string): Time[] => {
   if (times.length > 0 && times[times.length - 1].end === null) {
     const start = times[times.length - 1].start;
     if (calcDur(start, dt) > HESITATION_SECONDS) {
@@ -21,23 +21,23 @@ export function stopTimer(times: Time[], dt: string): Time[] {
     }
   }
   return times;
-}
+};
 
-export function toggleTimer(times: Time[], dt: string): Time[] {
+export const toggleTimer = (times: Time[], dt: string): Time[] => {
   if (times.length > 0 && times[times.length - 1].end === null) {
     times = stopTimer(times, dt);
   } else {
     times = startTimer(times, dt);
   }
   return times;
-}
+};
 
-export function calcElapsedTime(times: Time[]): number {
+export const calcElapsedTime = (times: Time[]): number => {
   const durationSecond = times
     .filter((time) => time.end !== null)
     .reduce((acc, time) => acc + calcDur(time["start"], time["end"]), 0);
   return Math.round(durationSecond / 60);
-}
+};
 
 export const adjustEnd = (times: Time[], minutes: number): Time[] => {
   if (times.length === 0) return times;

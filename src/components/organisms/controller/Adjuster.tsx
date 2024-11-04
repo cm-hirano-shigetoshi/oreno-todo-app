55d48f3242a69bf3720fa17fa5a438e310b792b4
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-11-04 22:44:54 +0900

    importを修正

--- src/components/organisms/controller/Adjuster.tsx
+++ src/components/organisms/controller/Adjuster.tsx
@@ -1,7 +1,8 @@
 import { memo, FC } from "react";
 import { Stack, HStack, Button } from "@chakra-ui/react";
 
-import { Todo, Project, isRunning } from "../../../logic/Todo";
+import { Todo, isRunning } from "../../../logic/Todo";
+import { Project } from "../../../logic/Project";
 import { StatusColor } from "../../../logic/List";
 
 const getProjectGeneralById = (
